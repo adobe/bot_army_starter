43d6e85b70d54616bde6ef842c0208603af07c30
Jeff Schomay <schomay@adobe.com>
2020-01-02 16:44:00 -0800

    use custom actions in the json tree
    update to latest bot army version to use extract actions in visual
    editor

--- lib/trees/sample_load.ex
+++ lib/trees/sample_load.ex
@@ -12,12 +12,16 @@ defmodule BotArmyStarter.Trees.SampleLoad do
 
   This uses the visual behavior tree editor
   (https://git.corp.adobe.com/BotTestingFramework/behavior_tree_editor) to define the
-  tree.
+  tree (which strips the custom action prefix BotArmyStarter.Actions, so it is added
+  back in).
   """
   def tree() do
     # "custom" runtime config is available from `BotArmy.SharedData`
     magic_number = BotArmy.SharedData.get(:magic_number)
 
-    BotArmy.BTParser.parse!("lib/trees/sample_load_bt.json", %{magic_number: magic_number})
+    BotArmy.BTParser.parse!("lib/trees/sample_load_bt.json",
+      context: %{magic_number: magic_number},
+      module_base: BotArmyStarter.Actions
+    )
   end
 end
