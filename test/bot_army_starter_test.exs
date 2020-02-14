e862abf7d1d6e8cfd5d758fd311c2fb368bff211
Jeff Schomay <schomay@adobe.com>
2020-02-14 13:51:54 -0800

    complete switch to using exunit to run integration tests

--- test/bot_army_starter_test.exs
+++ test/bot_army_starter_test.exs
@@ -1,7 +1,7 @@
 defmodule BotArmyStarterTest do
   @moduledoc false
 
-  use TODO
+  use BotArmy.IntegrationTest
 
   alias BotArmyStarter.Actions.Sample
 
@@ -12,21 +12,14 @@ defmodule BotArmyStarterTest do
     %{magic_number: 9}
   end
 
-  pre_tree
-  action(BotArmy.Actions, :log, ["Pre..."])
+  pre_all_tree "pre all" do
+    Node.sequence([action(BotArmy.Actions, :log, ["Pre all ..."])])
+  end
 
-  post_tree
-  action(BotArmy.Actions, :log, ["Pre..."])
+  post_all_tree "post all" do
+    BehaviorTree.Node.sequence([action(BotArmy.Actions, :log, ["Post all ..."])])
+  end
 
-  # TODO pre_all_tree
-  # TODO post_all_tree
-  # TODO pre_each_tree
-  # TODO post_each_tree
-  # TODO run_tree(tree) :: :ok | {:error, reason} function
-  # TODO move temp module into bot army
-  # TODO update readme in bot army
-
-  # @tag timeout: 6000
   # @tag :verbose
   test_tree "validate target number", context do
     Node.select([
@@ -35,28 +28,28 @@ defmodule BotArmyStarterTest do
     ])
   end
 
-  # TODO
-  test "Bots eventually guess correctly" do
+  # You can skip it if you want, see ExUnit.Case docs
+  # @tag :skip
+  test_tree "Bots eventually guess correctly" do
     # Warning, this is a recipe for an infinite loop.  In this case we are certain
     # the loop will exit eventually.
-    tree =
-      Node.sequence([
-        action(Sample, :init_guesses_count),
-        Node.repeat_until_succeed(
-          Node.sequence([
-            action(Sample, :make_guess),
-            action(Sample, :check_guess, [9])
-          ])
-        )
-      ])
+    Node.sequence([
+      action(Sample, :init_guesses_count),
+      Node.repeat_until_succeed(
+        Node.sequence([
+          action(Sample, :make_guess),
+          action(Sample, :check_guess, [9])
+        ])
+      )
+    ])
+  end
 
-    bot_mod = BotArmy.Bot.Default
-    timeout = 5000
-    test_name = "test 1"
-
-    Process.flag(:trap_exit, true)
-    {:ok, bot_pid} = BotArmy.Bot.start_link(bot_mod, id: test_name)
-    :ok = BotArmy.Bot.run(bot_pid, tree_with_done(tree))
-    assert_receive {:EXIT, ^bot_pid, :shutdown}, timeout
+  test_tree "validate target number using bt.json subtree", %{magic_number: magic_number} do
+    BotArmy.BTParser.parse!(
+      "lib/trees/sample_load_bt.json",
+      "validate_number",
+      context: %{magic_number: magic_number},
+      module_base: BotArmyStarter.Actions
+    )
   end
 end
